21863e93189cb871c9d6416da19665ecb6fb0a71
jcb121 <jcb.121@hotmail.co.uk>
2016-09-14 20:06:35 +0200

    added local server

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var gutil = require('gulp-util');
 var babelify = require('babelify');
+var connect = require('gulp-connect');
 
 // External dependencies you do not want to rebundle while developing,
 // but include in your application deployment
@@ -27,18 +28,29 @@ gulp.task('scripts', function () {
     bundleApp(false);
 });
 
+gulp.task('html', function(){
+	return gulp.src('src/*.html')
+	.pipe(gulp.dest('dist'));
+});
+
 gulp.task('deploy', function (){
 	bundleApp(true);
 });
 
 gulp.task('watch', function () {
-	gulp.watch(['./app/*.js'], ['scripts']);
+	gulp.watch(['./src/*.js'], ['scripts']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+		root: './dist'
+  });
 });
 
 // When running 'gulp' on the terminal this task will fire.
 // It will start watching for changes in every .js file.
 // If there's a change, the task 'scripts' defined above will fire.
-gulp.task('default', ['scripts','watch']);
+gulp.task('default', ['scripts', 'html', 'watch', 'connect']);
 
 // Private Functions
 // ----------------------------------------------------------------------------
@@ -47,7 +59,7 @@ function bundleApp(isProduction) {
 	// Browserify will bundle all our js files together in to one and will let
 	// us use modules in the front end.
 	var appBundler = browserify({
-    	entries: './app/app.js',
+    	entries: './src/app/app.js',
     	debug: true
   	})
 
@@ -63,7 +75,7 @@ function bundleApp(isProduction) {
 			.bundle()
 			.on('error', gutil.log)
 			.pipe(source('vendors.js'))
-			.pipe(gulp.dest('./web/js/'));
+			.pipe(gulp.dest('./dist/js/'));
   	}
   	if (!isProduction){
   		// make the dependencies external so they dont get bundled by the 
@@ -80,5 +92,5 @@ function bundleApp(isProduction) {
 	    .bundle()
 	    .on('error',gutil.log)
 	    .pipe(source('bundle.js'))
-	    .pipe(gulp.dest('./web/js/'));
+	    .pipe(gulp.dest('./dist/js/'));
 }
